7ecfde435a619fc5af9784465c507cbbe68e8ca1
Askar Temirbek <askartemir@gmail.com>
2025-09-03 15:29:14 -0400

    Imported view function into __init__.py

--- src/scsketch/__init__.py
+++ src/scsketch/__init__.py
@@ -14,3 +14,5 @@ class Widget(anywidget.AnyWidget):
     _esm = pathlib.Path(__file__).parent / "static" / "widget.js"
     _css = pathlib.Path(__file__).parent / "static" / "widget.css"
     value = traitlets.Int(0).tag(sync=True)
+
+from .directional import view
